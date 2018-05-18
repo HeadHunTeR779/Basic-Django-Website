930738a1e9c2918b0dba4a0a02644476d3cd064d
HeadHunTeR779 <dipankarjoy15@gmail.com>
2018-05-18 04:56:03 -0200

    Database connected to the admin-side AND template added for bravity

--- music/urls.py
+++ music/urls.py
@@ -2,5 +2,10 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+
+    # /music/
+    url(r'^$', views.index, name='index'),    #notice no () in function call
+
+    # /music/762/
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),            #notice no () in function call
 ]
