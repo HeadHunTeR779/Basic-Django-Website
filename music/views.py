407a0c2dc80bc29e0a293774b0f6e0f7210d13da
HeadHunTeR779 <dipankarjoy15@gmail.com>
2018-05-19 16:44:40 -0200

    Removed Loaded and added shortcut instead

--- music/views.py
+++ music/views.py
@@ -1,16 +1,13 @@
 from django.http import HttpResponse
-from django.template import loader
+from django.shortcuts import render
 from .models import Album
 
 
 
 def index(request):
     all_albums = Album.objects.all()
-    template = loader.get_template('music/index.html')
-    context = {
-        'all_albums': all_albums
-    }
-    return HttpResponse(template.render(context, request))
+    context = { 'all_albums': all_albums }
+    return render(request, 'music/index.html', context)  #note by default it thinks we r in templates folder
 
 def detail(request, album_id):
     return HttpResponse('<h2>Searching for album id: ' + str(album_id) + '</h2>')
