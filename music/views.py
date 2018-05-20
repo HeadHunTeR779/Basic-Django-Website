d46212482d23798c525dde1f10bd245554ef9286
HeadHunTeR779 <dipankarjoy15@gmail.com>
2018-05-20 02:14:26 -0200

    Raising the 404 error

--- music/views.py
+++ music/views.py
@@ -1,3 +1,4 @@
+from django.http import Http404
 from django.http import HttpResponse
 from django.shortcuts import render
 from .models import Album
@@ -6,8 +7,12 @@ from .models import Album
 
 def index(request):
     all_albums = Album.objects.all()
-    context = { 'all_albums': all_albums }
-    return render(request, 'music/index.html', context)  #note by default it thinks we r in templates folder
+    return render(request, 'music/index.html', { 'all_albums': all_albums })# note by default
+                                                            # it thinks we r in templates folder
 
 def detail(request, album_id):
-    return HttpResponse('<h2>Searching for album id: ' + str(album_id) + '</h2>')
+    try:
+        album = Album.objects.get(pk = album_id)
+    except Album.DoesNotExist:
+        raise Http404('Album Does Not Exist.')
+    return render(request, 'music/details.html', {'album' : album})
