ae2a4b4a9a270e31862458d3ead18d072e5bc78c
HeadHunTeR779 <dipankarjoy15@gmail.com>
2018-05-20 02:51:24 -0200

    Linked Song to Album

--- music/admin.py
+++ music/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Album            # dot for current note that directly "from models" wont work probably cuz he said __ini__.py file required to make it a package
+from .models import Album, Song            # dot for current note that directly "from models" wont work probably cuz he said __ini__.py file required to make it a package
 
 admin.site.register(Album)
+admin.site.register(Song)
+
