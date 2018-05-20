258fbf84286b6d6dc8eb4d5bcfaeb138827dc9e8
HeadHunTeR779 <dipankarjoy15@gmail.com>
2018-05-20 05:15:33 -0200

    Replaced Hardcoded URLs by Dynamics

--- music/urls.py
+++ music/urls.py
@@ -3,9 +3,10 @@ from . import views
 
 urlpatterns = [
 
-    # /music/
+    # /music/      === he is catching nothing
     url(r'^$', views.index, name='index'),    #notice no () in function call
 
-    # /music/762/
-    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),            #notice no () in function call
+    # /music/album_id/    == he is saying anything of the REGULAR EXPRESSION TYPE i.e. a nos after music/
+                        #and the number is saved in albun_id variable
+    url(r'^(?P<album_id>[0-9]+)/$', views.detail, name='detail'),  #notice no () in function call
 ]
