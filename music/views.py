b2dcf0ef0ad29f4b8580d96de6467954b2345b75
HeadHunTeR779 <dipankarjoy15@gmail.com>
2018-05-25 05:33:02 -0200

    Completed Favorites Functionality without changing the webpage

--- music/views.py
+++ music/views.py
@@ -1,6 +1,6 @@
 #from django.http import Http404
 from django.shortcuts import render, get_object_or_404
-from .models import Album
+from .models import Album, Song
 
 
 def index(request):
@@ -13,5 +13,18 @@ def detail(request, album_id):
     album = get_object_or_404(Album, pk=album_id)
     return render(request, 'music/details.html', {'album' : album})
 
-def favorite(request):
-    return
+def favorite(request, album_id):
+    album=get_object_or_404(Album, pk=album_id)
+    try:
+        selected_song = album.song_set.get(pk=request.POST['song'])
+    except (KeyError, Song.DoesNotExist):
+        return render(request, 'music/details.html', { 'error_message': 'Invalid Request' })
+    else:
+        
+        if (selected_song.is_favorite):
+            selected_song.is_favorite = False
+        else:
+            selected_song.is_favorite = True
+
+        selected_song.save()  #To echo to the data aboce line won't be saved IMPLICITLY!
+        return render(request, 'music/details.html', {'album' : album})
